7279bef4891751ab20cb19311736659724e92a47
Jorge Aguirre <83186748+jlammx@users.noreply.github.com>
2023-02-03 18:05:23 -0600

    Add 01-hola_mundo.dart

---
+++ bin/01-hola_mundo.dart
@@ -0,0 +1,13 @@
+void main() {
+
+  // Este es un comentario
+
+  /*
+   * Esto es un comentario multilínea
+   * Este también
+   * ................................
+   */
+
+  print('Hola Mundo!');
+
+}
