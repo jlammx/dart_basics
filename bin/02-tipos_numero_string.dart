25feec2a07520d9e7a49fd70ee7906896928bd1e
Jorge Aguirre <83186748+jlammx@users.noreply.github.com>
2023-02-03 21:19:18 -0600

    Add 02-tipos_numero_string.dart

---
+++ bin/02-tipos_numero_string.dart
@@ -0,0 +1,35 @@
+void main(L) {
+  
+// Strings
+
+  // Definición de variables flexibles (su valor de tipo de dato será asignado dependiendo el tipo de dato ingresado)
+  // var nombre = 'Tony';
+  // var apellido = 'Stark';
+
+  // Definición de variables por tipo de dato (recomendada)
+  // String nombre = 'Daenerys';
+  // String apellido = 'Targaryen';
+
+  // Declaración de variable constantes (su valor no cambia)
+  // final String nombre = 'Jorge';
+  final nombre = 'Jorge'; // La variable 'final' solo se puede configurar una vez.
+  const apellido = 'Targaryen'; // A las variables constantes no se les puede asignar un valor.
+
+  // nombre = 'Jorge';
+  
+  // Impresión normal
+  // print(nombre);
+
+  // Concatenación
+  print('$nombre $apellido');
+
+
+// Números
+
+  int empleados = 10;
+  double salario = 1800.00;
+
+  print('$empleados');
+  print('$salario');
+
+}
