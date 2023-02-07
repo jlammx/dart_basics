a9e26b3921e41ed19525a7a2c976c91a43ca8aca
Jorge Aguirre <83186748+jlammx@users.noreply.github.com>
2023-02-07 11:17:04 -0600

    Add 04-clase_basica.dart

---
+++ bin/07-clase_basica.dart
@@ -0,0 +1,47 @@
+void main() {
+  
+  // Declaración de una clase
+  // final Heroe wolverine = new Heroe();
+  // final wolverine = new Heroe('Daenery Targaryen', 'Dracarys');
+  // final wolverine = Heroe();
+  final wolverine = new Heroe(poder: 'Madre de Dragones', nombre: 'Daenerys');
+
+  // wolverine.nombre = 'Logan';
+  // wolverine.poder = 'Regeneración';
+
+  print(wolverine);
+}
+
+
+class Heroe{
+
+  // String? nombre;
+  // String? poder;
+  
+  String nombre;
+  String poder;
+  
+// Constructor (utiliza el mismo nombre de la clase)
+
+  // Forma incorrecta de un constructor en Dart
+/*   Heroe(String pNombre, String pPoder){
+    this.nombre = pNombre;
+    this.poder = pPoder;
+  } */
+
+  // Forma correcta de un constructor en Dart
+  // Heroe(this.nombre, this.poder);
+
+  // Enviar un constructor con argumentos por nombre (obligatoriamente)
+  Heroe({
+      required this.nombre, 
+      required this.poder
+  });
+  
+  // Mostrar las propiedades de la clase
+  @override
+  String toString(){
+    return 'Heroe: nombre: ${this.nombre}, poder: $poder';
+  }
+
+}
